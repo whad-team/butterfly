a2e7f1c11a96c6caf97df9745e60a9c7ae109338
Damien Cauquil <dcauquil@quarkslab.com>
2026-02-14 18:05:15 +0100

    Fixed byte order for initiator and advertiser addresses when crafting a Connected message notification.

--- src/controllers/blecontroller.cpp
+++ src/controllers/blecontroller.cpp
@@ -541,7 +541,7 @@ bool BLEController::stopConnection() {
 
 bool BLEController::connectionLost() {
 	//We are sending a notification to Host
-	//bsp_board_led_off(0);
+	bsp_board_led_off(0);
 
 	this->sendConnectionReport(DISCONNECTED);
 	this->sendConnectionReport(CONNECTION_LOST);
@@ -1854,20 +1854,20 @@ void BLEController::sendTriggeredReport(uint8_t id) {
 
 void BLEController::sendConnectedReport() {
 	uint8_t initiator[6] = {
-		this->own.bytes[5],
-		this->own.bytes[4],
-		this->own.bytes[3],
-		this->own.bytes[2],
+		this->own.bytes[0],
 		this->own.bytes[1],
-		this->own.bytes[0]
+		this->own.bytes[2],
+		this->own.bytes[3],
+		this->own.bytes[4],
+		this->own.bytes[5]
 	};
 	uint8_t responder[6] = {
-		this->connectionInitiationData.responder.bytes[0],
-		this->connectionInitiationData.responder.bytes[1],
-		this->connectionInitiationData.responder.bytes[2],
-		this->connectionInitiationData.responder.bytes[3],
+		this->connectionInitiationData.responder.bytes[5],
 		this->connectionInitiationData.responder.bytes[4],
-		this->connectionInitiationData.responder.bytes[5]
+		this->connectionInitiationData.responder.bytes[3],
+		this->connectionInitiationData.responder.bytes[2],
+		this->connectionInitiationData.responder.bytes[1],
+		this->connectionInitiationData.responder.bytes[0]
 
 	};
 
@@ -1903,12 +1903,12 @@ void BLEController::sendSlaveConnectedReport() {
     );
 
 	uint8_t initiator[6] = {
-		this->connectionInitiationData.responder.bytes[0],
-		this->connectionInitiationData.responder.bytes[1],
-		this->connectionInitiationData.responder.bytes[2],
-		this->connectionInitiationData.responder.bytes[3],
+		this->connectionInitiationData.responder.bytes[5],
 		this->connectionInitiationData.responder.bytes[4],
-		this->connectionInitiationData.responder.bytes[5]
+		this->connectionInitiationData.responder.bytes[3],
+		this->connectionInitiationData.responder.bytes[2],
+		this->connectionInitiationData.responder.bytes[1],
+		this->connectionInitiationData.responder.bytes[0]
 
 	};
 
@@ -2155,6 +2155,8 @@ void BLEController::connectionInitiationAdvertisementProcessing(BLEPacket *pkt) 
 void BLEController::connectionInitiationConnectedProcessing(BLEPacket *pkt) {
 	if (!this->sync) {
 		this->sync = true;
+
+        bsp_board_led_on(0);
 
         /* No empty PDUs. */
         this->setEmptyTransmitIndicator(false);
